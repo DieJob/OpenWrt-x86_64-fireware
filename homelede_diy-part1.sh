d287e9b621735f003dcf4d883efa3463533e85a9
DieJob <dudusprinkler@gmail.com>
2021-06-16 20:40:41 +0800

    Update homelede_diy-part1.sh

--- homelede_diy-part1.sh
+++ homelede_diy-part1.sh
@@ -16,5 +16,6 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-chmod +x ./prepareCompile.sh
-sh ./prepareCompile.sh
+cd openwrt
+chmod +x prepareCompile.sh
+sh prepareCompile.sh
